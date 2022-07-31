4bee1d59a7df37fb8238c52f2c61d4318b5dfb3b
Yuzhu Jiang <yuzhujiang0324@163.com>
2022-07-31 11:04:00 +0800

    Add an original draft of grader.

---
+++ Grader/grader.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_validate
+import xgboost as xgb
+
+CSV_FILE_PATH = './students_data_FIX.csv'
+
+''''''
+
+
+# Part 1 - data processing
+
+# TODO: target encoding
+def target_encoding(name, dataframe, m=1):
+    dataframe[name] = dataframe[name].str.split(";")
+    dataframe = dataframe.explode(name)
+    overall = dataframe["label"].mean()
+    dataframe = dataframe.groupby(name).agg(
+        freq=("label", "count"),
+        in_category=("label", np.mean)
+    ).reset_index()
+    dataframe["weight"] = dataframe["freq"] / (dataframe["freq"] + m)
+    dataframe["score"] = dataframe["weight"] * dataframe["in_category"] + (1 - dataframe["weight"]) * overall
+    return dataframe
+
+
+df = pd.read_csv(filepath_or_buffer=CSV_FILE_PATH)
+
+df_new = df.copy()
+# gender
+gender = df_new['gender'].value_counts()
+gender_map = dict((v, i) for i, v in enumerate(gender.index))
+df_new['gender'] = df_new.replace({'gender': gender_map})['gender']
+# Nationality
+nationality = df_new['Nationality'].value_counts()
+nationality_map = dict((v, i) for i, v in enumerate(nationality.index))
+df_new['Nationality'] = df_new.replace({'Nationality': nationality_map})['Nationality']
+# PlaceofBirth
+placeofBirth = df_new['PlaceofBirth'].value_counts()
+placeofBirth_map = dict((v, i) for i, v in enumerate(placeofBirth.index))
+df_new['PlaceofBirth'] = df_new.replace({'PlaceofBirth': placeofBirth_map})['PlaceofBirth']
+# StageID
+stageID = df_new['StageID'].value_counts()
+stageID_map = {'lowerlevel': 0, 'MiddleSchool': 1, 'HighSchool': 2}
+df_new['StageID'] = df_new.replace({'StageID': stageID_map})['StageID']
+# GradeID
+gradeID = df_new['GradeID'].value_counts()
+gradeID_map = {'G-02': 2, 'G-03': 3, 'G-04': 4, 'G-05': 5, 'G-06': 6, 'G-07': 7, 'G-08': 8,
+               'G-09': 9, 'G-10': 10, 'G-11': 11, 'G-12': 12}
+df_new['GradeID'] = df_new.replace({'GradeID': gradeID_map})['GradeID']
+# SectionID
+sectionID = df_new['SectionID'].value_counts()
+sectionID_map = {'A': 0, 'B': 1, 'C': 2}
+df_new['SectionID'] = df_new.replace({'SectionID': sectionID_map})['SectionID']
+# Topic
+topic = df_new['Topic'].value_counts()
+topic_map = dict((v, i) for i, v in enumerate(topic.index))
+df_new['Topic'] = df_new.replace({'Topic': topic_map})['Topic']
+# Semester
+semester = df_new['Semester'].value_counts()
+semester_map = dict((v, i) for i, v in enumerate(semester.index))
+df_new['Semester'] = df_new.replace({'Semester': semester_map})['Semester']
+# Relation
+relation = df_new['Relation'].value_counts()
+relation_map = dict((v, i) for i, v in enumerate(relation.index))
+df_new['Relation'] = df_new.replace({'Relation': relation_map})['Relation']
+# raisedhands
+# VisitedResources
+# AnnouncementsView
+# Discussion
+# ParentAnsweringSurvey
+parentAnsweringSurvey = df_new['ParentAnsweringSurvey'].value_counts()
+parentAnsweringSurvey_map = {'Yes': 1, 'No': 0}
+df_new['ParentAnsweringSurvey'] = \
+    df_new.replace({'ParentAnsweringSurvey': parentAnsweringSurvey_map})['ParentAnsweringSurvey']
+# ParentschoolSatisfaction
+parentschoolSatisfaction = df_new['ParentschoolSatisfaction'].value_counts()
+parentschoolSatisfaction_map = {'Bad': 0, 'Good': 1}
+df_new['ParentschoolSatisfaction'] = \
+    df_new.replace({'ParentschoolSatisfaction': parentschoolSatisfaction_map})['ParentschoolSatisfaction']
+# StudentAbsenceDays
+studentAbsenceDays = df_new['StudentAbsenceDays'].value_counts()
+studentAbsenceDays_map = {'Under-7': 0, 'Above-7': 1}
+df_new['StudentAbsenceDays'] = \
+    df_new.replace({'StudentAbsenceDays': studentAbsenceDays_map})['StudentAbsenceDays']
+# Class
+
+df_new.to_csv('./new_students_data_FIX.csv')
+
+# Part 2 - add new features
+# TODO
+
+# Part 3 - train models
+
+df_new = pd.read_csv(filepath_or_buffer='./new_students_data_FIX.csv')
+select_cols = ['StudentAbsenceDays', 'Relation', 'VisitedResources']
+# 'Topic', 'Semester', 'PlaceofBirth', 'raisedhands', 'StageID',
+# 'AnnouncementsView', 'Discussion', 'Nationality', 'SectionID',
+# 'ParentschoolSatisfaction', 'gender', 'GradeID', 'ParentAnsweringSurvey']
+X = df_new[select_cols]
+y = df_new['Class']
+
+param = {
+    "max_depth": 4,
+    "learning_rate": 0.1,
+    "n_estimators": 40,
+}
+estimator = xgb.sklearn.XGBRegressor(**param)
+scores = cross_validate(estimator, X=X, y=y, cv=5,
+                        return_train_score=True, return_estimator=True)
+
+print(scores['train_score'])
+print(scores['test_score'])
+for idx, estimator in enumerate(scores['estimator']):
+    print("Features sorted by their score for estimator {}:".format(idx))
+    feature_importances = pd.DataFrame(estimator.feature_importances_,
+                                       index=select_cols,
+                                       columns=['importance']).sort_values('importance', ascending=False)
+    print(feature_importances)
